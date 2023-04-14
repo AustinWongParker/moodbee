75ae98743c45104fefdbcad2dc38a4b3116a6c35
AustinWongParker <wongparker97@gmail.com>
2023-04-13 22:56:49 -0500

    templates and navbar

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
