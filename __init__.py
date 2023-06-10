8059082cb8eaee5d62c02f501eb0c3d775eb4bfa
AustinWongParker <wongparker97@gmail.com>
2023-06-10 13:57:17 -0500

    navbar and changing html pages

--- __init__.py
+++ __init__.py
@@ -4,11 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('ContactUs.html')
+
+@app.route('/how-it-works')
+def howitworks():
+    return render_template('How_it_works.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
