3d3c7c89b096bf8d3b024f317cd050e0f337f522
AustinWongParker <wongparker97@gmail.com>
2023-06-11 19:32:44 -0500

    template website loaded

--- __init__.py
+++ __init__.py
@@ -26,5 +26,10 @@ def howitworks():
 def testingPage():
     return render_template('testingPage.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
