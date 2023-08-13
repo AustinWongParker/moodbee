3f09b3804c7c819e010dffa45e3a5a630c3c89df
AustinWongParker <wongparker97@gmail.com>
2023-08-13 10:27:01 -0500

    trying some user auth stuff

--- __init__.py
+++ __init__.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, url_for, redirect, session, request
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = 'dev'
+
+class User(UserMixin):
+    def __init__(self, name, id, active=True):
+        self.name = name
+        self.id = id
+        self.active = active
+
+    def is_active(self):
+        # Here you should write whatever the code is
+        # that checks the database if your user is active
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
 
 @app.route('/')
 def hello_world():
